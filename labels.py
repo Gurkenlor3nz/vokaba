b8c2d5a5fad3adea2cda93cdbc11e4a6d3472417
Gurkenlor3nz <lorenzgurk3n@gmail.com>
2025-09-18 14:31:38 +0200

    welcome skript

---
+++ labels.py
@@ -0,0 +1,7 @@
+import os
+"""Variables"""
+vocab_path = "vocab"
+vocab_folder_content = os.listdir(vocab_path)
+
+
+"""Texte"""
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 """------Import Python packages------"""
 from datetime import datetime
+import os
+
+import labels
 
 """------Import kivy widgets------"""
 import kivy.core.window
@@ -12,6 +15,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.image import Image
 from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
 
 
 """------Import files------"""""
@@ -23,18 +27,25 @@ def log(text):
 
 class VocabpyApp(App):
     def build(self):
+        #Window init
         self.window = GridLayout()
         self.window.cols = 2
         self.window.rows = 2
 
-
         self.nein_text = Label(text="Nein")
         self.window.add_widget(self.nein_text)
 
+        for i in labels.vocab_folder_content:
+            if os.path.isfile(os.path.join(labels.vocab_path, i)):
+                voc_stacks = Button(text=i, size_hint_y=None, height=40)
+                voc_stacks.bind(on_release=lambda instance, fname=i: self.on_file_select(log("datei ausgewählt")))
+                self.window.add_widget(voc_stacks)
 
         self.settings_button = Button(text="Settings")
         self.settings_button.bind(on_press=self.test)
         self.window.add_widget(self.settings_button)
+
+
 
         return self.window
 
